fc1c0db93e704d288507732ba7cc0530e72bc4c6
ShadowD1987 <dildiazme@unal.edu.co>
2024-07-31 14:17:46 -0500

    Add files via upload

--- EjeircciosMatrices.py
+++ EjeircciosMatrices.py
@@ -10,4 +10,17 @@ def promedio_arreglo(x):
 
     return p/len(x)
 
-print(promedio_arreglo([5,4,7,8,5]))
+print(promedio_arreglo([5,4,7,8,5]))    
+
+#2. Desarrolle un algoritmo que calcule el punto de dos arreglos de numeros enteros que sean de la misma longitud
+
+def product_punto(v,w):
+    
+    if len (v) !=  len(w):
+        
+        raise ValueError ("los arreglos no son de la misma longitud")
+    
+    producto = sum(v1 * w1 for v1, w1 in zip(v,w))
+    return producto
+
+print(product_punto([1,2,3],[4,5,6]))
