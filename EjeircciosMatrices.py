14cd18f468e6815945df83fc525b632c37c8e766
kennyvargasUN <kevargasr@unal.edu.co>
2024-07-31 15:43:39 -0500

    Add files via upload

--- EjeircciosMatrices.py
+++ EjeircciosMatrices.py
@@ -23,6 +23,24 @@ def product_punto(v,w):
     producto = sum(v1 * w1 for v1, w1 in zip(v,w))
     return print(f"el punto de los arreglos es ", producto)
 
+#3. Desarrolla un algoritmo que calcule el producto directo de dos arreglos de numeros
+#reales de igual tamaño
+
+def producto_directo(x,y):
+
+    z = []
+
+    if len(x) == len(y):
+        
+        for i in range(0,len(x)):
+
+            z.append(x[i]*y[i])
+        
+        print("El resultado del producto directo de los dos arreglos es ", z)
+        
+    
+    else:
+        print("los arreglos deben tener el mismo numero de terminos")
 
 
 def main():
@@ -30,6 +48,8 @@ def main():
      promedio_arreglo([5,4,7,8,5])
      
      product_punto([1,2,3],[4,5,6])
+
+     producto_directo([2,4,6,8],[2,3,5,10])
      
      
 if __name__ == "__main__":
